139aacb45410399eef4429c4a0b378b32e8bafaa
thefakeartist <73249738+thefakeartist@users.noreply.github.com>
2020-10-25 06:14:55 -0500

    a revison
    played with some more code and tweaked af ew things

--- script.js
+++ script.js
@@ -47,3 +47,6 @@ function showSlides(n) {
   dots[slideIndex-1].className += " active";
   captionText.innerHTML = dots[slideIndex-1].alt;
 }
+
+var x = "Is the browser online? " + navigator.onLine;
+
